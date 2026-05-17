8011f2a6c280da7d247047abcddb4c6d1e278070
Kridhatuli23 <gulatikarishma@gmail.com>
2026-05-17 18:40:29 +0530

    Type_casting

---
+++ Data_types/Type_casting.py
@@ -0,0 +1,19 @@
+name="Kridha"
+age = 13
+is_student= True 
+weight= 45.5
+print("Name:", name)
+print("Data type of name is: ", type(name))
+print("Age: ", age)
+print("Data type of age is: ", type(age))
+print("is_student: ", is_student)
+print("Data type of is_student is: ", type(is_student))
+print("Weight: ", weight )
+print("Data type of weight is: ", type(weight))
+
+print("\n After type casting...")
+age = str(age)
+print(age)
+print("The data type of age is: ", type(age))
+weight = int(weight)
+print("The data type of weight is:", type(weight))
