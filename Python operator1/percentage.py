590e0a0de2b80c493ade4bbbfbf313bb386aa2dd
Kridhatuli23 <gulatikarishma@gmail.com>
2026-06-04 19:08:56 +0530

    percentage

---
+++ Python operator1/percentage.py
@@ -0,0 +1,8 @@
+mark1=float(input("Please enter English marks:"))
+mark2=float(input("Please enter Maths marks:"))
+mark3=float(input("Please enter Science marks:"))
+mark4=float(input("Please enter Social Studies marks:"))
+sum = mark1+mark2+mark3+mark4
+print("The sum of the marks obtained in the subjects=",sum)
+perc = (sum/400)*100
+print("The percentage is",perc)
