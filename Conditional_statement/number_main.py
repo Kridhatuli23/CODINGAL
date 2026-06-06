cc29f15621f19d8de779d21fa4b876ee058bd68e
Kridhatuli23 <gulatikarishma@gmail.com>
2026-06-06 19:02:55 +0530

    number_main

---
+++ Conditional_statement/number_main.py
@@ -0,0 +1,7 @@
+num1= float(input("Please enter a number:"))
+if num1>15:
+    print(num1,"is greater than 15")
+    print("It belongs to if block")
+else:
+    print(num1,"is smaller than 15")
+    print("it belongs to else block")
