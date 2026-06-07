e51c9c2d58d452ec20467174fe4072f38d8bdf76
Kridhatuli23 <gulatikarishma@gmail.com>
2026-06-07 19:10:06 +0530

    not operator

---
+++ Python_operator2/not.py
@@ -0,0 +1,24 @@
+a=100
+b=356
+c=356
+print(a!=b)
+print(b!=c)
+
+
+
+a="python"
+b="codingal"
+if a!=b:
+    print(a,"and",b,"are different.")
+
+
+
+a=67
+b=98
+if (a==1)!=(b==5):
+    print("Hello world")
+
+
+a=int(input("Enter a number:"))
+if a%2!=0:
+    print(a,"is odd number")
