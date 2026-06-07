5513b3d257ca9b552d87401f4f077388f43d8aaa
Kridhatuli23 <gulatikarishma@gmail.com>
2026-06-07 18:14:27 +0530

    storing values

---
+++ Homework/Storing_values.py
@@ -0,0 +1,10 @@
+Rahul= "26th July, 2012"
+Nikhil="11th February, 2012"
+Nityant="30th April, 2012"
+Atharv="19th December, 2012"
+Abhrant="02 October, 2012"
+print("Rahul's birthdate is", Rahul)
+print("Nihil's birthdate is", Nikhil)
+print("Nityant's birthdate is", Nityant)
+print("Atharv's birthdate is", Atharv)
+print("Abhrant's birthdate is", Abhrant)
---
+++ Homework/square_root.py
@@ -0,0 +1,3 @@
+num=int(input("Please enter an integer:"))
+sq= num
+print(sq,"is the square root of",num)
