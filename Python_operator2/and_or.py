5930ad49d15d9f9627c5bbf77433b7565ccbc8f1
Kridhatuli23 <gulatikarishma@gmail.com>
2026-06-07 18:55:43 +0530

    and-or

---
+++ Python_operator2/and_or.py
@@ -0,0 +1,23 @@
+a=-10
+b=0
+c=103
+if a and b and c:
+    print("All the numbers have boolean value as True.")
+else:
+    print("Atleast one number has boolean value as false.")
+
+
+
+
+a=-2
+b=13
+c=966
+if a>0 or b>0:
+    print("Either of the numbers is greater than 0.")
+else: 
+    print("No number is greater than 0.")
+
+if c>0 or b>0:
+    print("Either of the numbers is greater than 0")
+else:
+    print("No number is greater than 0.")
