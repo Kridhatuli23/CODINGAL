7dfb996e4b39e661adb999aea9670b82dafbc04d
Kridhatuli23 <gulatikarishma@gmail.com>
2026-06-08 10:11:10 +0530

    summer_times

--- Homework/square_root.py
+++
@@ -1,3 +0,0 @@
-num=int(input("Please enter an integer:"))
-sq= num
-print(sq,"is the square root of",num)
---
+++ Homework/summer_times.py
@@ -0,0 +1,5 @@
+temp=float(input("Please enter the current temperature in degree celcuis:"))
+if temp<=15:
+    print("The temperature is low as it is below 15 degree celsius, so you should wear a jacket or a pullover.")
+else:
+    print("The temperature is high as it is above 15 degree celsius, so you should wear light clothes.")
