eb1886d4baf7d103baf9531ca7fba061cc9f181c
Kridhatuli23 <gulatikarishma@gmail.com>
2026-06-09 19:08:11 +0530

    identity

---
+++ Boilerplate/boil.py
@@ -0,0 +1,11 @@
+a=23
+print(type(a))
+
+b=5.99999
+print(type(b))
+
+c="kridha is a good girl"
+print(type(c))
+
+d=False
+print(type(d))
---
+++ operator_3/identity.py
@@ -0,0 +1,21 @@
+x=6
+if(type(x) is int):
+    print("True")
+else:
+    print("False")
+
+
+x=9.7644
+if(type(x)is not float):
+    print("True")
+else:
+    print("false")
+
+x=678
+y=678
+if(x is y):
+    print("x and y have same identity")
+
+y=3690
+if(x is not y):
+    print("x and y have different identity")
