11bedac52f1ec86aa29d90c7f79510c29e2d4a82
Kridhatuli23 <gulatikarishma@gmail.com>
2026-06-11 18:57:31 +0530

    divisble

---
+++ Python_challengers/divisible_numbers.py
@@ -0,0 +1,6 @@
+num1=float(input("Enter a number as numerator: "))
+num2=float(input("Enter a number as denominator: "))
+if num1%num2==0:
+    print(num1,"is divible by",num2)
+else:
+    print(num1,"is not divible by",num2)
