501f855d2d801d115c6c72c6b8971d18ab1ba720
Kridhatuli23 <gulatikarishma@gmail.com>
2026-06-16 19:01:56 +0530

    act1

---
+++ nested_conditions/car.py
@@ -0,0 +1,9 @@
+print("Select your ride:")
+print("1.car")
+print("2.bike")
+choice=int(input("Enter your choice:"))
+if(choice ==1):
+    print("What type of bike?")
+    print("1.scooty")
+    print("2.scooter")
+    choice2=int(input("Enter your choice2:"))
