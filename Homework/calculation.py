291afba7a1735fd64a79b35329c7a13ca95a61bf
Kridhatuli23 <gulatikarishma@gmail.com>
2026-06-28 16:58:50 +0530

    power

---
+++ Homework/calculation.py
@@ -0,0 +1,5 @@
+num=int(input("Please enter the number you want to calculate power of:"))
+n=int(input("Enter the number depicting the number of powers you want to calculate:"))
+for i in range(1,n+1):
+    power=num**i
+    print(power)
